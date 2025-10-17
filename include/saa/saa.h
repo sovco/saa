fd0d9713c47cc956f2ce91b19f655280722aad29
sovco <no@email.stop>
2025-10-17 19:25:45 +0200

    first commit

---
+++ include/saa/saa.h
@@ -0,0 +1,123 @@
+#ifndef SAA_H
+#define SAA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+
+
+typedef struct saa_arena_page_t saa_arena_page;
+typedef struct saa_arena_t saa_arena;
+
+struct saa_arena_page_t
+{
+    char *data;
+    saa_arena_page *next;
+    size_t size;
+};
+
+struct saa_arena_t
+{
+    saa_arena_page *pages;
+    const size_t page_size;
+};
+
+static inline saa_arena saa_arena_create(const size_t size);
+static inline void *saa_arena_push(saa_arena *arena, size_t lenght);
+static inline double *saa_arena_push_value_double(saa_arena *arena, double value);
+static inline float *saa_arena_push_value_float(saa_arena *arena, float value);
+static inline int *saa_arena_push_value_int(saa_arena *arena, int value);
+static inline bool *saa_arena_push_value_bool(saa_arena *arena, int value);
+static inline char *saa_arena_push_value_string(saa_arena *arena, const char *value);
+static inline void *saa_arena_push_arbitrary(saa_arena *arena, void *value, size_t lenght);
+static inline void saa_arena_destroy(saa_arena *arena);
+
+#ifdef __cplusplus
+}// extern "C"
+#endif
+
+#ifdef SAA_IMPL
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <string.h>
+
+static inline saa_arena_page *__saa_allocate_arena_page(const size_t page_size)
+{
+    saa_arena_page *ret = malloc(sizeof(*ret));
+    *ret = (saa_arena_page){ .data = (char *)malloc(sizeof(char) * page_size), .size = 0, .next = NULL };
+    return ret;
+}
+
+static inline saa_arena saa_arena_create(const size_t page_size)
+{
+    return (saa_arena){ .page_size = page_size, .pages = __saa_allocate_arena_page(page_size) };
+}
+
+static inline void *saa_arena_push(saa_arena *arena, size_t lenght)
+{
+    void *ret_ptr = NULL;
+    saa_arena_page *latest_page = NULL;
+    for (latest_page = arena->pages; latest_page->next != NULL; latest_page = latest_page->next) {}
+    if (latest_page->size + lenght > arena->page_size) {
+        latest_page->next = __saa_allocate_arena_page(arena->page_size);
+        latest_page = latest_page->next;
+    }
+    ret_ptr = (void *)(latest_page->data + latest_page->size);
+    latest_page->size += lenght;
+    return ret_ptr;
+}
+
+static inline void *saa_arena_push_arbitrary(saa_arena *arena, void *value, size_t lenght)
+{
+    void *ptr = saa_arena_push(arena, lenght);
+    memcpy(ptr, value, lenght);
+    return ptr;
+}
+
+static inline double *saa_arena_push_value_double(saa_arena *arena, double value)
+{
+    return (double *)saa_arena_push_arbitrary(arena, (void *)&value, sizeof(value));
+}
+
+static inline float *saa_arena_push_value_float(saa_arena *arena, float value)
+{
+    return (float *)saa_arena_push_arbitrary(arena, (void *)&value, sizeof(value));
+}
+
+static inline int *saa_arena_push_value_int(saa_arena *arena, int value)
+{
+    return (int *)saa_arena_push_arbitrary(arena, (void *)&value, sizeof(value));
+}
+
+static inline bool *saa_arena_push_value_bool(saa_arena *arena, int value)
+{
+    return (bool *)saa_arena_push_arbitrary(arena, (void *)&value, sizeof(value));
+}
+
+static inline char *saa_arena_push_value_string(saa_arena *arena, const char *value)
+{
+    return (char *)saa_arena_push_arbitrary(arena, (void *)value, strlen(value) + 1);
+}
+
+static inline void saa_arena_destroy(saa_arena *arena)
+{
+    saa_arena_page *page = arena->pages;
+    while (page != NULL) {
+        saa_arena_page *tmp = page->next;
+        free(page->data);
+        free(page);
+        page = tmp;
+    }
+}
+
+#ifdef __cplusplus
+}// extern "C"
+#endif
+
+#endif// SAA_IMPL
+#endif// SAA_H
---
+++ test/saa-test.c
@@ -0,0 +1,105 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stf/stf.h>
+#define SAA_IMPL
+#include <saa/saa.h>
+
+STF_TEST_CASE(saa, create_arena_destroy_arena)
+{
+    static const size_t arena_page_size = 100;
+    saa_arena arena = saa_arena_create(arena_page_size);
+    STF_EXPECT(arena.pages != NULL, .failure_msg = "pages are NULL");
+    saa_arena_destroy(&arena);
+}
+
+STF_TEST_CASE(saa, create_arena_push_some_bytes_destroy)
+{
+    static const size_t arena_page_size = 100;
+    static const size_t push_size = 50;
+    saa_arena arena = saa_arena_create(arena_page_size);
+    STF_EXPECT(arena.pages != NULL, .failure_msg = "pages are NULL");
+    char *pushed = saa_arena_push(&arena, push_size);
+    STF_EXPECT(pushed != NULL, .failure_msg = "pushing did not return a valid pointer");
+    saa_arena_destroy(&arena);
+}
+
+STF_TEST_CASE(saa, create_arena_push_some_bytes_write_destroy)
+{
+    static const size_t arena_page_size = 100;
+    static const size_t push_size = sizeof(double);
+    static const double value = 77.7;
+    saa_arena arena = saa_arena_create(arena_page_size);
+    STF_EXPECT(arena.pages != NULL, .failure_msg = "pages are NULL");
+    double *pushed = saa_arena_push(&arena, push_size);
+    STF_EXPECT(pushed != NULL, .failure_msg = "pushing did not return a valid pointer");
+    memcpy(pushed, &value, sizeof(double));
+    STF_EXPECT(*pushed == value, .failure_msg = "written value did not match");
+    saa_arena_destroy(&arena);
+}
+
+STF_TEST_CASE(saa, arena_pushing_100_bytes_on_100_byte_page_does_not_create_another_page)
+{
+    static const size_t arena_page_size = 100;
+    static const size_t push_size = 50;
+    saa_arena arena = saa_arena_create(arena_page_size);
+    STF_EXPECT(arena.pages != NULL, .failure_msg = "pages are NULL");
+    char *pushed = saa_arena_push(&arena, push_size);
+    STF_EXPECT(pushed != NULL, .failure_msg = "pushing did not return a valid pointer");
+    pushed = NULL;
+    pushed = saa_arena_push(&arena, push_size);
+    STF_EXPECT(pushed != NULL, .failure_msg = "pushing did not return a valid pointer");
+    STF_EXPECT(arena.pages->next == NULL, .failure_msg = "pushing 100 bytes was not supposed to create a new page");
+    saa_arena_destroy(&arena);
+}
+
+STF_TEST_CASE(saa, arena_pushing_2_doubles)
+{
+    static const size_t arena_page_size = sizeof(double) * 2;
+    static const double push_value_a = 77.7;
+    static const double push_value_b = 777.7;
+    saa_arena arena = saa_arena_create(arena_page_size);
+    STF_EXPECT(arena.pages != NULL, .failure_msg = "pages are NULL");
+    double *pushed_a = saa_arena_push_value_double(&arena, push_value_a);
+    STF_EXPECT(pushed_a != NULL, .failure_msg = "pushed_a is NULL");
+    STF_EXPECT(*pushed_a == push_value_a, .failure_msg = "pushed_a value did not match");
+    double *pushed_b = saa_arena_push_value_double(&arena, push_value_b);
+    STF_EXPECT(pushed_b != NULL, .failure_msg = "pushed_b is NULL");
+    STF_EXPECT(*pushed_b == push_value_b, .failure_msg = "pushed_b value did not match");
+    STF_EXPECT(arena.pages->next == NULL, .failure_msg = "pushing two doubles was not suposed to create another page");
+    saa_arena_destroy(&arena);
+}
+
+STF_TEST_CASE(saa, arena_pushing_string)
+{
+    static const size_t arena_page_size = 100;
+    static const char *push_string = "yo, this be a string";
+    saa_arena arena = saa_arena_create(arena_page_size);
+    STF_EXPECT(arena.pages != NULL, .failure_msg = "pages are NULL");
+    char *pushed = saa_arena_push_value_string(&arena, push_string);
+    STF_EXPECT(strcmp(pushed, push_string) == 0, .failure_msg = "string don't match");
+    saa_arena_destroy(&arena);
+}
+
+STF_TEST_CASE(saa, arena_pushing_150_bytes_on_100_byte_page_does_create_another_page)
+{
+    static const size_t arena_page_size = 100;
+    static const size_t push_size = 50;
+    saa_arena arena = saa_arena_create(arena_page_size);
+    STF_EXPECT(arena.pages != NULL, .failure_msg = "pages are NULL");
+    char *pushed = saa_arena_push(&arena, push_size);
+    STF_EXPECT(pushed != NULL, .failure_msg = "pushing did not return a valid pointer");
+    pushed = NULL;
+    pushed = saa_arena_push(&arena, push_size);
+    STF_EXPECT(pushed != NULL, .failure_msg = "pushing did not return a valid pointer");
+    STF_EXPECT(arena.pages->next == NULL, .failure_msg = "second page is not NULL");
+    pushed = NULL;
+    pushed = saa_arena_push(&arena, push_size);
+    STF_EXPECT(pushed != NULL, .failure_msg = "pushing did not return a valid pointer");
+    STF_EXPECT(arena.pages->next != NULL, .failure_msg = "second page is NULL");
+    saa_arena_destroy(&arena);
+}
+
+int main(void)
+{
+    return STF_RUN_TESTS();
+}
